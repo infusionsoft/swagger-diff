110998c57c969a390a5ccaf5a282cf0193c467bf
Ross Beamish <bigshadow@gmail.com>
2017-02-06 11:09:05 -0700

    indent

--- src/main/java/com/deepoove/swagger/diff/compare/PropertyDiff.java
+++ src/main/java/com/deepoove/swagger/diff/compare/PropertyDiff.java
@@ -11,52 +11,52 @@ import java.util.Map;
 
 public class PropertyDiff {
 
-    private List<ElProperty> increased;
-    private List<ElProperty> missing;
+      private List<ElProperty> increased;
+      private List<ElProperty> missing;
 
-    Map<String, Model> oldDedinitions;
-    Map<String, Model> newDedinitions;
+      Map<String, Model> oldDedinitions;
+      Map<String, Model> newDedinitions;
 
-    private PropertyDiff() {
-        increased = new ArrayList<ElProperty>();
-        missing = new ArrayList<ElProperty>();
-    }
+      private PropertyDiff() {
+            increased = new ArrayList<ElProperty>();
+            missing = new ArrayList<ElProperty>();
+      }
 
-    public static PropertyDiff buildWithDefinition(Map<String, Model> left,
-                                                   Map<String, Model> right) {
-        PropertyDiff diff = new PropertyDiff();
-        diff.oldDedinitions = left;
-        diff.newDedinitions = right;
-        return diff;
-    }
+      public static PropertyDiff buildWithDefinition(Map<String, Model> left,
+                                                     Map<String, Model> right) {
+            PropertyDiff diff = new PropertyDiff();
+            diff.oldDedinitions = left;
+            diff.newDedinitions = right;
+            return diff;
+      }
 
-    public PropertyDiff diff(Property left, Property right) {
-        if ((null == left || left instanceof RefProperty) && (null == right || right instanceof RefProperty)) {
-            Model leftModel = null == left ? null : oldDedinitions.get(((RefProperty) left).getSimpleRef());
-            Model rightModel = null == right ? null : newDedinitions.get(((RefProperty) right).getSimpleRef());
-            ModelDiff diff = ModelDiff
-                    .buildWithDefinition(oldDedinitions, newDedinitions)
-                    .diff(leftModel, rightModel);
-            increased.addAll(diff.getIncreased());
-            missing.addAll(diff.getMissing());
-        }
-        return this;
-    }
+      public PropertyDiff diff(Property left, Property right) {
+            if ((null == left || left instanceof RefProperty) && (null == right || right instanceof RefProperty)) {
+                  Model leftModel = null == left ? null : oldDedinitions.get(((RefProperty) left).getSimpleRef());
+                  Model rightModel = null == right ? null : newDedinitions.get(((RefProperty) right).getSimpleRef());
+                  ModelDiff diff = ModelDiff
+                          .buildWithDefinition(oldDedinitions, newDedinitions)
+                          .diff(leftModel, rightModel);
+                  increased.addAll(diff.getIncreased());
+                  missing.addAll(diff.getMissing());
+            }
+            return this;
+      }
 
-    public List<ElProperty> getIncreased() {
-        return increased;
-    }
+      public List<ElProperty> getIncreased() {
+            return increased;
+      }
 
-    public void setIncreased(List<ElProperty> increased) {
-        this.increased = increased;
-    }
+      public void setIncreased(List<ElProperty> increased) {
+            this.increased = increased;
+      }
 
-    public List<ElProperty> getMissing() {
-        return missing;
-    }
+      public List<ElProperty> getMissing() {
+            return missing;
+      }
 
-    public void setMissing(List<ElProperty> missing) {
-        this.missing = missing;
-    }
+      public void setMissing(List<ElProperty> missing) {
+            this.missing = missing;
+      }
 
 }
--- src/test/java/com/deepoove/swagger/test/SwaggerDiffTest.java
+++ src/test/java/com/deepoove/swagger/test/SwaggerDiffTest.java
@@ -14,99 +14,99 @@ import java.util.List;
 
 public class SwaggerDiffTest {
 
-    final String SWAGGER_V1_DOC = "http://petstore.swagger.io/v2/swagger.json";
-    // String swagger_v1_doc = "petstore_v1.json";
-    final String SWAGGER_V2_DOC = "petstore_v2.json";
+      final String SWAGGER_V1_DOC = "http://petstore.swagger.io/v2/swagger.json";
+      // String swagger_v1_doc = "petstore_v1.json";
+      final String SWAGGER_V2_DOC = "petstore_v2.json";
 
-    final String SWAGGER_EMPTY_DOC = "petstore_empty.json";
+      final String SWAGGER_EMPTY_DOC = "petstore_empty.json";
 
-    @Test
-    public void testEqual() {
-        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V1_DOC);
-        List<Endpoint> newEndpoints = diff.getNewEndpoints();
-        List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
-        List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-        Assert.assertTrue(newEndpoints.isEmpty());
-        Assert.assertTrue(missingEndpoints.isEmpty());
-        Assert.assertTrue(changedEndPoints.isEmpty());
-    }
+      @Test
+      public void testEqual() {
+            SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V1_DOC);
+            List<Endpoint> newEndpoints = diff.getNewEndpoints();
+            List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
+            List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
+            Assert.assertTrue(newEndpoints.isEmpty());
+            Assert.assertTrue(missingEndpoints.isEmpty());
+            Assert.assertTrue(changedEndPoints.isEmpty());
+      }
 
-    @Test
-    public void testNewApi() {
-        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_EMPTY_DOC, SWAGGER_V1_DOC);
-        List<Endpoint> newEndpoints = diff.getNewEndpoints();
-        List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
-        List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-        String html = new HtmlRender("Changelog",
-                "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-                .render(diff);
+      @Test
+      public void testNewApi() {
+            SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_EMPTY_DOC, SWAGGER_V1_DOC);
+            List<Endpoint> newEndpoints = diff.getNewEndpoints();
+            List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
+            List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
+            String html = new HtmlRender("Changelog",
+                    "http://deepoove.com/swagger-diff/stylesheets/demo.css")
+                    .render(diff);
 
-        try {
-            FileWriter fw = new FileWriter(
-                    "src/test/resources/testNewApi.html");
-            fw.write(html);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Assert.assertTrue(newEndpoints.size() > 0);
-        Assert.assertTrue(missingEndpoints.isEmpty());
-        Assert.assertTrue(changedEndPoints.isEmpty());
-    }
+            try {
+                  FileWriter fw = new FileWriter(
+                          "src/test/resources/testNewApi.html");
+                  fw.write(html);
+                  fw.close();
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
+            Assert.assertTrue(newEndpoints.size() > 0);
+            Assert.assertTrue(missingEndpoints.isEmpty());
+            Assert.assertTrue(changedEndPoints.isEmpty());
+      }
 
-    @Test
-    public void testDeprecatedApi() {
-        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_EMPTY_DOC);
-        List<Endpoint> newEndpoints = diff.getNewEndpoints();
-        List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
-        List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-        String html = new HtmlRender("Changelog",
-                "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-                .render(diff);
+      @Test
+      public void testDeprecatedApi() {
+            SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_EMPTY_DOC);
+            List<Endpoint> newEndpoints = diff.getNewEndpoints();
+            List<Endpoint> missingEndpoints = diff.getMissingEndpoints();
+            List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
+            String html = new HtmlRender("Changelog",
+                    "http://deepoove.com/swagger-diff/stylesheets/demo.css")
+                    .render(diff);
 
-        try {
-            FileWriter fw = new FileWriter(
-                    "src/test/resources/testDeprecatedApi.html");
-            fw.write(html);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Assert.assertTrue(newEndpoints.isEmpty());
-        Assert.assertTrue(missingEndpoints.size() > 0);
-        Assert.assertTrue(changedEndPoints.isEmpty());
-    }
+            try {
+                  FileWriter fw = new FileWriter(
+                          "src/test/resources/testDeprecatedApi.html");
+                  fw.write(html);
+                  fw.close();
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
+            Assert.assertTrue(newEndpoints.isEmpty());
+            Assert.assertTrue(missingEndpoints.size() > 0);
+            Assert.assertTrue(changedEndPoints.isEmpty());
+      }
 
-    @Test
-    public void testDiff() {
-        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V2_DOC);
-        List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
-        String html = new HtmlRender("Changelog",
-                "http://deepoove.com/swagger-diff/stylesheets/demo.css")
-                .render(diff);
+      @Test
+      public void testDiff() {
+            SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V2_DOC);
+            List<ChangedEndpoint> changedEndPoints = diff.getChangedEndpoints();
+            String html = new HtmlRender("Changelog",
+                    "http://deepoove.com/swagger-diff/stylesheets/demo.css")
+                    .render(diff);
 
-        try {
-            FileWriter fw = new FileWriter(
-                    "src/test/resources/testDiff.html");
-            fw.write(html);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Assert.assertFalse(changedEndPoints.isEmpty());
-    }
+            try {
+                  FileWriter fw = new FileWriter(
+                          "src/test/resources/testDiff.html");
+                  fw.write(html);
+                  fw.close();
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
+            Assert.assertFalse(changedEndPoints.isEmpty());
+      }
 
-    @Test
-    public void testDiffAndMarkdown() {
-        SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V2_DOC);
-        String render = new MarkdownRender().render(diff);
-        try {
-            FileWriter fw = new FileWriter(
-                    "src/test/resources/testDiff.md");
-            fw.write(render);
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+      @Test
+      public void testDiffAndMarkdown() {
+            SwaggerDiff diff = SwaggerDiff.compareV2(SWAGGER_V1_DOC, SWAGGER_V2_DOC);
+            String render = new MarkdownRender().render(diff);
+            try {
+                  FileWriter fw = new FileWriter(
+                          "src/test/resources/testDiff.md");
+                  fw.write(render);
+                  fw.close();
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
+      }
 }
